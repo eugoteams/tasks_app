058df5834f4245aa269d654897ef727c8ff8052c
maturi karthik <maturikarthik@maturis-MacBook-Pro.local>
2024-01-18 01:13:08 +0530

    adding animation

--- src/Component/DisplayTask/DisplayTask.js
+++ src/Component/DisplayTask/DisplayTask.js
@@ -1,16 +1,27 @@
 /** @format */
 
 import React, { Fragment, useContext } from "react";
+import Lottie from "react-lottie";
+import anim from "../../assets/lottie/anim.json";
 import style from "./DisplayTask.module.css";
 import Card from "../Card/Card";
 import { AppContext } from "../../Store/AppContext";
-import { PlusSquare } from "lucide-react";
+import { PlusSquare, Plus } from "lucide-react";
+import Chip from "../Chip/Chip";
 
 const DisplayTask = (props) => {
   const { tasksObject, dispatch } = useContext(AppContext);
   let tasks = tasksObject["tasks"];
   let openPopUp = tasksObject["openPopUp"];
   let layout = tasksObject["layout"];
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: anim,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   const onActionSelectListener = (action, indexRecv) => {
     console.log("Action Payload reacv", action, indexRecv);
@@ -41,9 +52,9 @@ const DisplayTask = (props) => {
 
   return (
     <Fragment>
-      <div className={`${style.grid} ${style[`${layout}`]}`}>
-        {tasks.length > 0 ? (
-          tasks.map((task, index) => {
+      {tasks.length > 0 ? (
+        <div className={`${style.grid} ${style[`${layout}`]}`}>
+          {tasks.map((task, index) => {
             return (
               <Card
                 key={`tasks_${index}`}
@@ -58,16 +69,26 @@ const DisplayTask = (props) => {
                 layout={layout}
               />
             );
-          })
-        ) : (
-          <div className={`${style.message}`}>
-            <div className={`${style.inner_message}`}>
-              <PlusSquare size={24} absoluteStrokeWidth />
-              <span>Your tasks are empty</span>
-            </div>
+          })}
+        </div>
+      ) : (
+        <div className={`${style.message}`}>
+          <div className={`${style.inner_message}`}>
+            <Lottie options={defaultOptions} height={200} width={200} />
+            <span
+              style={{
+                margin: "1rem auto",
+              }}
+            >
+              Your have zero tasks
+            </span>
+            <Chip>
+              <Plus size={18} absoluteStrokeWidth color="#fffffe" />
+              <p>create Task</p>
+            </Chip>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </Fragment>
   );
 };
